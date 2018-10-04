9c3b4705fb77499e34027745f744d0d098e59277
Michael DOHR <dohr.michael@gmail.com>
2018-10-04 07:52:38 +0200

    Update

--- errors/errors.go
+++ errors/errors.go
@@ -5,3 +5,9 @@ type notFoundError struct{}
 func (*notFoundError) Error() string { return "Not Found" }
 
 var NotFoundError error = &notFoundError{}
+
+type illegalEntityError struct{ message string }
+
+func (c *illegalEntityError) Error() string { return c.message }
+
+func IllegalEntityError(message string) error { return &illegalEntityError{message} }
